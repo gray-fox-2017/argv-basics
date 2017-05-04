bd78b815a19de76ec103f619684023f6e988b6cb
stedyyulius <stedyyulius@gmail.com>
2017-05-04 13:22:14 +0700

    done

--- pig_latin.js
+++ pig_latin.js
@@ -1,26 +1,60 @@
-'use strict'
+// 'use strict'
+// 
+// let pigLatin = (word) => {
+//   let firstLetter = word.charAt(0);
+//   if(isPowel(firstLetter)){
+//     return word
+//   }else{
+//     return `${word.substr(1)}${firstLetter}ay`;
+//   }
+// }
+// 
+// let isPowel = (char) => {
+//   return (/^[aeiou]$/i).test(char);
+// }
+// 
+// let convert = (sentence) => {
+//   let result = []
+//   let words = sentence.trim().split(/\s+/g)
+//   for (let i=0;i<words.length;i++) {
+//     result[i] = pigLatin(words[i])
+//   }
+// 
+//   return result.join(" ")
+// }
+"use strict"
+function pigLatin(word){
+  let vowel = /[aiueo]/i
+  let vowelmatch = word.match(vowel)
 
-let pigLatin = (word) => {
-  let firstLetter = word.charAt(0);
-  if(isPowel(firstLetter)){
-    return word
-  }else{
-    return `${word.substr(1)}${firstLetter}ay`;
+  if (vowelmatch.index === 0){
+      return word;
   }
+
+  if (vowelmatch.index !== 0) {
+    let firstvowel = vowelmatch.index;
+    var pig = word.substr(firstvowel) + word.substr(0,firstvowel) + "ay";
+  }
+
+
+
+  return pig;
 }
 
-let isPowel = (char) => {
-  return (/^[aeiou]$/i).test(char);
-}
-
-let convert = (sentence) => {
+function pigLatin2(sentence) {
   let result = []
-  let words = sentence.trim().split(/\s+/g)
-  for (let i=0;i<words.length;i++) {
-    result[i] = pigLatin(words[i])
+  let split = sentence.split(" ")
+  for (let i = 0; i < split.length; i++){
+    result.push(pigLatin(split[i]))
   }
-
   return result.join(" ")
 }
 
-// Your CLI code here
+var pigread = process.argv
+
+var convert = ""
+    for (let x = 2; x < pigread.length; x++){
+      convert += pigLatin(pigread[x]) + " "}
+console.log(convert)
+
+
