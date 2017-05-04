ae7a4598117903b3d1d83f06cbf4166e12dbbd94
hakiemaul <noersedya@gmail.com>
2017-05-04 19:30:48 +0700

    argv basics done

--- pig_latin.js
+++ pig_latin.js
@@ -1,26 +1,34 @@
 'use strict'
 
-let pigLatin = (word) => {
-  let firstLetter = word.charAt(0);
-  if(isPowel(firstLetter)){
-    return word
-  }else{
-    return `${word.substr(1)}${firstLetter}ay`;
+function pigLatin(sentence) {
+  let arr=sentence.split(' ');
+
+  for(let i=0;i<arr.length;i++) {
+    let word = arr[i]
+    let firstCharacterIsVowel = /^[aiueo]/g.test(word)
+    if(!firstCharacterIsVowel){
+      let vowel = word.match(/[aiueo]\w+/g);
+      let cons  = word.replace(vowel,'')
+      if(vowel!==null) {
+        word = vowel+cons+'ay';
+      } else {
+        word += 'ay';
+      }
+    }
+    arr[i] = word;
   }
-}
-
-let isPowel = (char) => {
-  return (/^[aeiou]$/i).test(char);
-}
-
-let convert = (sentence) => {
-  let result = []
-  let words = sentence.trim().split(/\s+/g)
-  for (let i=0;i<words.length;i++) {
-    result[i] = pigLatin(words[i])
-  }
-
-  return result.join(" ")
+  return arr.join(' ');
 }
 
 // Your CLI code here
+
+let cli = process.argv;
+
+let input = [];
+for(let i = 2; i < cli.length; i++) {
+  input.push(cli[i]);
+}
+
+input = input.join(' ');
+
+console.log(pigLatin(input))
